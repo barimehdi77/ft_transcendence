39f9d03657579bad86d9aa17dcf4dfa3c94cb85f
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-07-30 15:17:56 +0100

    Tiny change

--- front-end/pages/profile.tsx
+++ front-end/pages/profile.tsx
@@ -1,3 +1,4 @@
+import ProfileInfoItem from '../components/profileInfoItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faMedal,
@@ -15,11 +16,11 @@ const Profile = () => {
 			<div className='profile drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'>
 				<div className='header bg-sky-800 flex items-center px-10 py-4 rounded-t-3xl	'>
 					<div className='header-info text-neutral-100'>
-						<h2 className='text-xl font-semibold inline'>Abderr***</h2>
+						<h2 className='text-xl font-semibold inline'>Abderr</h2>
 						<FontAwesomeIcon icon={faMedal} className='text-teal-500' />
-						<p>asfaihi@student.1337.ma***</p>
+						<p>asfaihi@student.1337.ma</p>
 					</div>
-					<div className='avatar w-16'>
+					<div className='avatar w-16 ml-5'>
 						<img
 							src='/download.jpeg'
 							alt='User avatar'
@@ -29,15 +30,20 @@ const Profile = () => {
 				</div>
 
 				<div className='main-info bg-white px-10 py-4 rounded-b-3xl'>
-					<div className='item flex items-center'>
-						<div className='bg-white w-8 h-8 flex items-center justify-center mr-3 rounded-full drop-shadow-[0_0px_4px_rgba(0,0,0,0.25)]'>
-							<FontAwesomeIcon icon={faUser} />
-						</div>
-						<div className=''>
-							<p className='text-sm text-neutral-500'>Full Name</p>
-							<h2 className='font-semibold'>Abderrahmane Sfaihi***</h2>
-						</div>
-					</div>
+					<ProfileInfoItem
+						icon={faUser}
+						field='Full Name'
+						info='Abderrahmane Sfaihi'
+					/>
+					<ProfileInfoItem icon={faTag} field='Login' info='asfaihi' />
+					<ProfileInfoItem icon={faCoins} field='Points' info='42 Pts' />
+					<ProfileInfoItem
+						icon={faTableTennisPaddleBall}
+						field='Games Played'
+						info='33'
+					/>
+					<ProfileInfoItem icon={faCircleCheck} field='Wins' info='25' />
+					<ProfileInfoItem icon={faCircleXmark} field='Losses' info='8' />
 				</div>
 			</div>
 		</main>
