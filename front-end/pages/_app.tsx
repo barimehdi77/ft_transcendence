6c1600de78f6ff0c93d87ec9bec646bd55993307
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-08-02 17:16:10 +0100

    I don't even know anymore

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-// import Layout from '../components/layout';
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	
 	return (
-		// <Layout>
+		<Layout>
 			<Component {...pageProps} />
-		// </Layout>
+		</Layout>
 	);
 }
 
