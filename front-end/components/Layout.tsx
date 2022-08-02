4045c96314925829a7d830ea432f1de5461dc11b
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-08-02 16:58:09 +0100

    Trying something

--- front-end/components/Layout.tsx
+++ front-end/components/Layout.tsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from 'react';
 import Navbar from './navbar';
 
 const Layout = ({ children }: any) => {
-	const [isLogged, setIsLogged] = useState(false);
-	useEffect(() => {
-		const isLoggedIn = () => {
-			console.log(localStorage.getItem('token'));
-			if (localStorage.getItem('token')) {
-				console.log('ksnfdons');
-				setIsLogged(true);
-			} else setIsLogged(false);
-		};
-		isLoggedIn();
-	}, [isLogged]);
+	// const [isLogged, setIsLogged] = useState(false);
+	// useEffect(() => {
+	// 	const isLoggedIn = () => {
+	// 		console.log(localStorage.getItem('token'));
+	// 		if (localStorage.getItem('token')) {
+	// 			console.log('ksnfdons');
+	// 			setIsLogged(true);
+	// 		} else setIsLogged(false);
+	// 	};
+	// 	isLoggedIn();
+	// }, [isLogged]);
 	return (
 		<div className='bg-hero-pattern bg-cover bg-center'>
-			{isLogged ? <Navbar /> : null}
+			{/* {isLogged ? <Navbar /> : null} */}
 			<>{children}</>
 		</div>
 	);
--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -11,4 +11,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 	);
 }
 
+
 export default MyApp;
