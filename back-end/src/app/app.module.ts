171aa99575c87d48722f2bc4f2704a5d6c27ddd3
barimehdi77 <mbari@student.1337.ma>
2022-08-04 11:57:24 -0400

    fix some bugs

--- back-end/src/app/app.module.ts
+++ back-end/src/app/app.module.ts
@@ -33,7 +33,7 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(validateUserMiddleware)
-      .exclude('/auth')
+      .exclude('/auth/*')
       .forRoutes('*');
   }
 }
