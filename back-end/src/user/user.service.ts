6f6836c748a2acc5208cef31313b2a64f2a24644
barimehdi77 <mbari@student.1337.ma>
2022-08-05 11:15:39 -0400

    buuugs

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -113,7 +113,7 @@ export class UserService {
     console.log("this is the data", data, "auth", auth);
     const User = await this.findUserName(data.user_name as string);
     console.log(User);
-    if (User) return null;
+    if (User === null) return null;
 
     return this.prisma.user.update({
       where: {
