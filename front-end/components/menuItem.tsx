259148e92d0ada0042e6f2469bf1c87639f6723c
DarkSide77 <mbari@student.1337.ma>
2022-08-05 12:13:44 +0100

    Delete front-end directory

--- front-end/components/menuItem.tsx
+++
@@ -1,9 +0,0 @@
-const MenuItem = (props: any) => {
-	return (
-		<li className="text-white text-xl font-semibold ml-14">
-			<a href={props.path}>{props.path}</a>
-		</li>
-	);
-};
-
-export default MenuItem;
