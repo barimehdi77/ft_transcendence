6c3ae6dc4f8d66b251c5d2bd966e3bbcdc8b36de
barimehdi77 <mbari@student.1337.ma>
2022-08-09 21:28:25 +0100

    delete middleware for now

--- back-end/src/app/app.module.ts
+++ back-end/src/app/app.module.ts
@@ -10,12 +10,12 @@ import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { validateUserMiddleware } from 'src/comman/middleware/ValidateUser.middleware';
+// import { validateUserMiddleware } from 'src/comman/middleware/ValidateUser.middleware';
 import { UserService } from 'src/user/user.service';
 import { PrismaService } from './prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ProfileModule } from 'src/profile/profile.module';
-import { ValidateComplateProfileMiddleware } from 'src/comman/middleware/ValidateCompleteProfile.middleware';
+// import { ValidateComplateProfileMiddleware } from 'src/comman/middleware/ValidateCompleteProfile.middleware';
 
 @Module({
   imports: [
@@ -30,16 +30,15 @@ import { ValidateComplateProfileMiddleware } from 'src/comman/middleware/Validat
   controllers: [AppController],
   providers: [AppService, UserService, PrismaService, JwtService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(validateUserMiddleware)
-      .exclude('api/auth/login', 'api/auth/redirect')
-      .forRoutes('*');
-    consumer
-      .apply(ValidateComplateProfileMiddleware)
-      .exclude('api/auth/login', 'api/auth/redirect', 'api/user', 'api/user/setup')
-      .forRoutes('*');
-  }
+export class AppModule {}//implements NestModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(validateUserMiddleware)
+  //     .forRoutes('user', 'profile', '');
+  //   consumer
+  //     .apply(ValidateComplateProfileMiddleware)
+  //     .exclude('api/auth/login', 'api/auth/redirect', 'api/user', 'api/user/setup')
+  //     .forRoutes('*');
+  // }
 
-}
+// }
--- back-end/src/comman/middleware/ValidateCompleteProfile.middleware.ts
+++ back-end/src/comman/middleware/ValidateCompleteProfile.middleware.ts
@@ -3,18 +3,19 @@ import { NextFunction, Request, Response } from "express";
 import { UserService } from "src/user/user.service";
 
 
-@Injectable()
-export class ValidateComplateProfileMiddleware implements NestMiddleware {
-	constructor(private readonly userService: UserService) {}
-	async use(req: Request, res: Response, next: NextFunction) {
-		const token = req.headers.authorization.replace("Bearer undefined","") as string;
-		if (token === "") throw new HttpException("unauthorized", 401);
-		const user = await this.userService.FindUser(req.headers.authorization);
-		if (!user) {
-			throw new UnauthorizedException();
-		}
-		req.user = user;
-		if (user.profile_done === false) throw new HttpException("uncompleted", 477);
-		next();
-	}
-}
+// @Injectable()
+// export class ValidateComplateProfileMiddleware implements NestMiddleware {
+// 	constructor(private readonly userService: UserService) {}
+// 	async use(req: Request, res: Response, next: NextFunction) {
+// 		console.log("this is not good");
+// 		const token = req.headers.authorization.replace("Bearer undefined","") as string;
+// 		if (token === "") throw new HttpException("unauthorized", 401);
+// 		const user = await this.userService.FindUser(req.headers.authorization);
+// 		if (!user) {
+// 			throw new UnauthorizedException();
+// 		}
+// 		req.user = user;
+// 		if (user.profile_done === false) throw new HttpException("uncompleted", 477);
+// 		next();
+// 	}
+// }
