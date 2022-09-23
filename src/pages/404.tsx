a84b6a3e06758669c832e6ec436a436cea938e73
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-09-23 09:54:16 +0100

    Custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+	return (
+		<main className='min-h-screen flex flex-col justify-center items-center'>
+			<h1 className='text-sky-800 font-extrabold text-9xl mb-4'>404</h1>
+			<h2 className='font-bold text-4xl'>Page does not exist</h2>
+		</main>
+	);
+}
+
+export default NotFound;
